a4d4fe6139e336fdaa8e2789ed3eee083c91f494
Nikita Popov <nikic@php.net>
2017-05-09 13:41:04 +0200

    Make project build on Windows

---
+++ src/FileUtil.cpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "FileUtil.hpp"
+#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+namespace OpenABL {
+
+bool directoryExists(const std::string &name) {
+  struct stat info;
+  return stat(name.c_str(), &info) == 0 && (info.st_mode & S_IFDIR);
+}
+
+bool createDirectory(const std::string &name) {
+  if (directoryExists(name)) {
+    return true;
+  }
+
+#ifdef _WIN32
+  return CreateDirectory(name.c_str(), NULL) != 0;
+#else
+  return mkdir(name.c_str(), 0755) == 0;
+#endif
+}
+
+void writeToFile(const std::string &name, const std::string &contents) {
+  std::ofstream f(name);
+  f << contents;
+}
+
+void copyFile(const std::string &from, const std::string &to) {
+  std::ifstream src(from);
+  std::ofstream dst(to);
+  dst << src.rdbuf();
+}
+
+void makeFileExecutable(const std::string &name) {
+#ifndef _WIN32
+  chmod(name.c_str(), 0755);
+#endif
+}
+
+}
--- src/FileUtil.hpp
+++ src/FileUtil.hpp
@@ -1,39 +1,13 @@
 #pragma once
 
-#include <fstream>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <string>
 
 namespace OpenABL {
 
-static inline bool directoryExists(const std::string &name) {
-  struct stat info;
-  return stat(name.c_str(), &info) == 0 && (info.st_mode & S_IFDIR);
-}
-
-static inline bool createDirectory(const std::string &name) {
-  if (directoryExists(name)) {
-    return true;
-  }
-
-  // TODO Windows?
-  return mkdir(name.c_str(), 0755) == 0;
-}
-
-static inline void writeToFile(const std::string &name, const std::string &contents) {
-  std::ofstream f(name);
-  f << contents;
-}
-
-static inline void copyFile(const std::string &from, const std::string &to) {
-  std::ifstream src(from);
-  std::ofstream dst(to);
-  dst << src.rdbuf();
-}
-
-static inline void makeFileExecutable(const std::string &name) {
-  // TODO Windows?
-  chmod(name.c_str(), 0755);
-}
+bool directoryExists(const std::string &name);
+bool createDirectory(const std::string &name);
+void writeToFile(const std::string &name, const std::string &contents);
+void copyFile(const std::string &from, const std::string &to);
+void makeFileExecutable(const std::string &name);
 
 }
