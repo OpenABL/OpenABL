fb4cab3c045e9d3db4ef2f64225dcebcfc88c07c
Nikita Popov <nikita.ppv@gmail.com>
2017-09-27 16:48:57 +0200

    Handle FlameGPU buffer size in bench.py
    Again, temporary solution

--- bench/bench.py
+++ bench/bench.py
@@ -12,7 +12,7 @@ if not os.path.isfile(openabl_bin):
     print("OpenABL binary not found at " + openabl_bin)
     sys.exit(1)
 
-def openabl_run(model, backend, params):
+def openabl_run(model, backend, params, config):
     model_file = example_dir + '/' + model + '.abl'
     args = [
         openabl_bin,
@@ -26,14 +26,21 @@ def openabl_run(model, backend, params):
         args.append('-P')
         args.append(key + '=' + str(value))
 
+    for key, value in config.items():
+        args.append('-C')
+        args.append(key + '=' + str(value))
+
     return subprocess.check_output(args)
 
-def openabl_get_exec_time(model, backend, params):
-    output = openabl_run(model, backend, params)
+def openabl_get_exec_time(model, backend, params, config):
+    output = openabl_run(model, backend, params, config)
     matches = re.search('Execution time: (.*)s', output)
     if matches is None:
         raise RuntimeError('Failed to extract execution time')
     return float(matches.group(1))
+
+def next_pow2(n):
+    return 2**(n-1).bit_length()
 
 if len(sys.argv) < 3:
     print('Usage: bench.py backend model')
@@ -55,7 +62,12 @@ while num_agents <= max_num_agents:
         'num_agents': num_agents,
     }
 
-    exec_time = openabl_get_exec_time(model, backend, params)
+    # TODO Find a better solution for this
+    config = {}
+    if backend == 'flamegpu':
+        config['flamegpu.buffer_size'] = next_pow2(num_agents)
+
+    exec_time = openabl_get_exec_time(model, backend, params, config)
     print(str(num_agents) + ',' + str(exec_time))
 
     num_agents *= num_agents_factor
