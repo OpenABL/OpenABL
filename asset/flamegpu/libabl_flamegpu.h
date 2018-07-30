c1019e0cd6fcb6273871be8f497936569930da05
Nikita Popov <nikita.ppv@gmail.com>
2018-07-30 16:59:56 +0200

    Use CONTINUOUS rng on FlameGPU

--- asset/flamegpu/libabl_flamegpu.h
+++ asset/flamegpu/libabl_flamegpu.h
@@ -2,11 +2,11 @@
 #define LIBABL_FLAMEGPU_H
 
 static inline __device__ float random(RNG_rand48 *rand48, float min, float max) {
-	return min + rnd(rand48) * (max - min);
+	return min + rnd<CONTINUOUS>(rand48) * (max - min);
 }
 
 static inline __device__ int randomInt(RNG_rand48 *rand48, int min, int max) {
-	return min + (int) (rnd(rand48) * (max - min + 1));
+	return min + (int) (rnd<CONTINUOUS>(rand48) * (max - min + 1));
 }
 
 #endif
